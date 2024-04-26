f356bcd8a3beeb61fb5c62cc4f585693d24fe30e
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2024-04-26 17:05:28 +0200

    add instruction count to blocks

--- viz/vizlib/cfg_script.js
+++ viz/vizlib/cfg_script.js
@@ -22,7 +22,6 @@ const formatIrInstructions = (ir) => {
 const openBlockDetail = (node) => {
     window.scrollTo(0, 0)
     $("div#block-info").show()
-    console.log(node)
     let dataNodeId = `data_${$(node).attr("id")}`
     let dataNode = $(`#${dataNodeId}`)
     $("div#block-ir").html(formatIrInstructions($(dataNode).attr("ir")))
