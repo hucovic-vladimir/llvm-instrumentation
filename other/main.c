e7136d0d2f0b8d80e23ac0c31b40a43e9714540a
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2024-05-02 16:17:26 +0200

    change the main.c listing

--- other/main.c
+++ other/main.c
@@ -1,6 +1,7 @@
 int global_variable = 0;
 
 int main(void) {
-	int local_variable = 10;
-	return local_variable + 5;
+	int local_variable = 5;
+	global_variable = 10 + local_variable;
+	return 0;
 }
