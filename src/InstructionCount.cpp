0f4773b3b22f2cb25e3877330da935d1b2b52181
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2024-05-08 10:08:38 +0200

    remove dead code

--- src/InstructionCount.cpp
+++ src/InstructionCount.cpp
@@ -32,44 +32,13 @@
 
 namespace fs = std::filesystem;
 
-
 using blockWrapperMap = std::unordered_map<BasicBlock*, BasicBlockWrapper*>;
-blockWrapperMap wrappers;
 
-/// @todo Move elsewhere
-/// @todo rename
 using blockCovers = std::map<BasicBlock*, std::set<BasicBlock*>>;
 
 const std::string getFileName(const std::string& path);
 
-void enqueueSuccessors(BasicBlock* currentBlock, std::vector<BasicBlock*>& blockQueue) {
-	for(BasicBlock* succ : successors(currentBlock)) {
-		if(std::find(blockQueue.begin(), blockQueue.end(), succ) == blockQueue.end()) {
-			blockQueue.push_back(succ);
-		}
-	}
-}
-
-void markBranchBlockCovered(BasicBlock* branchingBlock, blockCovers &bc) {
-	for(BasicBlock* succ : successors(branchingBlock)) {
-		bc[succ].insert(branchingBlock);
-	}
-}
-
-
-std::vector<BasicBlock*> checkAllBlocksCovered(std::vector<BasicBlock*> covered, std::vector<BasicBlock*> allBlock) {
-	std::vector<BasicBlock*> uncovered;
-	for(BasicBlock* block : allBlock) {
-		if(std::find(covered.begin(), covered.end(), block) == covered.end()) {
-			uncovered.push_back(block);
-		}
-	}
-	return uncovered;
-}
-
-void doNothing() {
-	return;
-}
+blockWrapperMap wrappers;
 
 const std::string getLocalArrayName(Module &M) {
 	std::string arrayName = "__basicblocks_arr_" + getFileName(M.getSourceFileName());
@@ -156,29 +125,6 @@ const std::string getBasicBlockModuleName(BasicBlock& BB) {
 	return module->getSourceFileName();
 }
 
-/// @todo move elsewhere
-/// @brief Get the basic block information to be exported from the pass
-/// @param BB The basic block to get the information from
-/// @param bbIndex The ID of the basic block
-const std::string getBBInfo(BasicBlock& BB, unsigned long bbIndex){
-	unsigned long id = bbIndex;
-	std::string moduleName = getBasicBlockModuleName(BB);
-	std::string functionName = getBasicBlockDemangledFunctionName(BB);
-	std::string label = BB.getName().str() != "" ? BB.getName().str() : "NO_LABEL";
-	size_t size = BB.sizeWithoutDebug();
-	std::pair<int, int> startEndLines = PassUtilities::getBasicBlockStartEndLines(BB);
-
-	std::stringstream ss;
-	ss << id << "," << moduleName <<  "," << functionName << "," << label << "," 
-		 << size  << "," << startEndLines.first << "," << startEndLines.second << "\n";
-
-	return ss.str();
-}
-
-bool doesFunctionContainLoops(LoopInfo &LI) {
-	auto loops = LI.getLoopsInPreorder();
-	return !loops.empty();
-}
 
 std::vector<OptimizationPattern*> getOptimizationPatterns(Function &F) {
 	std::vector<OptimizationPattern*> patterns;
@@ -198,6 +144,8 @@ std::vector<OptimizationPattern*> getOptimizationPatterns(Function &F) {
 			}
 		}
 		processed.push_back(bb);
+
+		/// check for diamond pattern
 		DiamondPattern* diamond = DiamondPattern::checkForPattern(wrappers, wrappers[bb], processed);
 		if(diamond) {
 			patterns.push_back(diamond);
@@ -208,6 +156,8 @@ std::vector<OptimizationPattern*> getOptimizationPatterns(Function &F) {
 			}
 			continue;
 		}
+
+		/// check for half diamond pattern
 		HalfDiamondPattern* halfDiamond = HalfDiamondPattern::checkForPattern(wrappers, wrappers[bb], processed);
 		if(halfDiamond) {
 			patterns.push_back(halfDiamond);
@@ -218,6 +168,8 @@ std::vector<OptimizationPattern*> getOptimizationPatterns(Function &F) {
 			}
 			continue;
 		}
+
+		/// check for unconditional jump pattern
 		UnconditionalJumpPattern* unconditionalJump = UnconditionalJumpPattern::checkForPattern(wrappers, wrappers[bb], processed);
 		if(unconditionalJump) {
 			patterns.push_back(unconditionalJump);
@@ -242,15 +194,12 @@ std::vector<OptimizationPattern*> getOptimizationPatterns(Function &F) {
 /// @return The preserved analyses (IR is modified, so none to be safe)
 PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 
-	// TODO remove this and load the ignored modules from a file probably
-	if(M.getName() == "conftest.c") { return PreservedAnalyses::none(); }
 	unsigned long bbCount = 0;
 	LLVMContext& CTX = M.getContext();
 
 	InstrumentationFunctions IF = InstrumentationFunctions(CTX);
 
 	std::vector<FunctionPatterns*> patterns;
-
 
 	std::vector<BasicBlockWrapper*> wrappersVec;
 	for(auto &F : M) {
@@ -268,9 +217,7 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 
 	std::error_code EC;
 	std::string sourceFileName = getFileName(M.getSourceFileName());
-	errs() << "Source file name: " << sourceFileName << "\n";
 	std::string directories = fs::path(M.getSourceFileName()).parent_path().string();
-	errs() << "Directories: " << directories << "\n";
 	if(directories.size())
 		fs::create_directories(".basicblocks/" + directories);
 	raw_fd_ostream bbFile(".basicblocks/" + M.getSourceFileName() + ".json", EC);
@@ -291,12 +238,9 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 
 	for(auto &F : M){
 		if(F.isDeclaration()) continue;
-		SmallVector<BasicBlock*> coveredBlocks;
-		FunctionPassManager FPM;
-		FPM.addPass(RequireAnalysisPass<LoopAnalysis, Function>());
-		FunctionAnalysisManager &FAM = MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
-		FPM.run(F, FAM);
 
+		/// get the basic block patterns in this function and add them to the patterns vector
+		/// use the nonInstrumentedBlocks to specify which blocks should not be instrumented
 		std::vector<OptimizationPattern*> funcPatterns = getOptimizationPatterns(F);
 		std::vector<BasicBlock*> nonInstrumentedBlocks;
 		for(auto& pattern : funcPatterns) {
@@ -306,6 +250,7 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 			}
 		}
 
+		/// If the entry block is not part of any pattern, check for the sum of exits pattern
 		if(std::find(nonInstrumentedBlocks.begin(), nonInstrumentedBlocks.end(), &F.getEntryBlock()) == nonInstrumentedBlocks.end()) {
 			SumOfExitsPattern* sumOfExits = SumOfExitsPattern::checkForPattern(wrappers, wrappers[&F.getEntryBlock()], nonInstrumentedBlocks);
 			if(sumOfExits) {
@@ -321,6 +266,7 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 		for(auto &BB : F){
 			Instruction* insertionPoint = &*BB.getFirstInsertionPt();
 
+			/// instrument the basic block
 			if(std::find(nonInstrumentedBlocks.begin(), nonInstrumentedBlocks.end(), &BB) == nonInstrumentedBlocks.end()) {
 				incrementCounter(M, insertionPoint, wrappers[&BB]->getId());
 			}
@@ -352,28 +298,19 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 	} // for F
 
 
-	/// dump the instrumented module to a file in the .llfiles directory
-	/// mostly for debugging purposes - could be turned on or off in compilation
-	raw_fd_ostream llFileStream(".llfiles/" + getFileName(M.getName().str()) + ".ll", EC);
-	if(EC){
-		std::cerr << "Failed to open " << M.getName().str() << " for writing" << "\n";
-		exit(1);
-	}
-	M.print(llFileStream, nullptr);
-
+	/// write the name of the module, the name of the array and the number of basic blocks to modules.tmp
 	std::fstream arraysFile("./modules.tmp", std::ios::out | std::ios::app);	
 	if(!arraysFile.is_open()){
-		std::cerr << "Failed to open moduleArrays for writing" << "\n";
+		std::cerr << "Failed to open modules.tmp for writing" << "\n";
 		exit(1);
 	}
 	arraysFile << M.getSourceFileName() << "," << getLocalArrayName(M) << "," << bbCount << "\n";
 
 
+	/// export patterns
 	if(patterns.size() > 0){
 		std::string sourceFileName = getFileName(M.getSourceFileName());
-		errs() << "Source file name: " << sourceFileName << "\n";
 		std::string directories = fs::path(M.getSourceFileName()).parent_path().string();
-		errs() << "Directories: " << directories << "\n";
 		if(directories.size())
 			fs::create_directories(".patterns/" + directories);
 		raw_fd_ostream patternFile(".patterns/" + directories + "/" + sourceFileName + ".json", EC);
@@ -391,7 +328,7 @@ PreservedAnalyses InstructionCount::run(Module &M, ModuleAnalysisManager &MAM){
 
 	return PreservedAnalyses::none();
 }
-/// @brief Get the plugin info for the pass
+/// @brief Static entry point
 PassPluginLibraryInfo getInstructionCountPluginInfo(){
 	const auto callback = [](PassBuilder &PB) {
 		PB.registerOptimizerLastEPCallback(
@@ -405,7 +342,7 @@ PassPluginLibraryInfo getInstructionCountPluginInfo(){
 
 
 
-/// @brief Register the pass with the pass manager
+/// @brief Dynamic entry point
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo(){
 	return getInstructionCountPluginInfo();
